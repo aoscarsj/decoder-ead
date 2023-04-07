0468a6efb188d05fccd4d843c4aa8d2a36a8985c
Artur Oscar <Arturoscar3@gmail.com>
2023-04-07 20:01:13 -0300

    [authuser]: adding mapping for course service

---
+++ authuser-service/src/main/kotlin/authuser/common/const/ConfigServerMappingConstants.kt
@@ -0,0 +1,6 @@
+package authuser.common.const
+
+//TODO: ADD SERVICE REGISTRY
+const val COURSE_SERVICES_URI = "http://localhost:8082"
+
+const val COURSE_SERVICES_COURSES_BASE_RESOURCE = "/courses/users/{userId}"
