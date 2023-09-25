62e3431a418befe9cc94cf2446a6e41526d3cdc1
Artur Oscar <Arturoscar3@gmail.com>
2023-09-25 20:47:47 -0300

    [authuser]: registering authuser in service registry.

--- authuser-service/build.gradle.kts
+++ authuser-service/build.gradle.kts
@@ -12,7 +12,7 @@ group = "com.decoder.authuser"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
-extra["springCloudVersion"] = "2021.0.6"
+extra["springCloudVersion"] = "2021.0.8"
 
 repositories {
     mavenCentral()
@@ -31,6 +31,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.amqp:spring-rabbit-test")
--- authuser-service/src/main/kotlin/authuser/AuthuserServiceApplication.kt
+++ authuser-service/src/main/kotlin/authuser/AuthuserServiceApplication.kt
@@ -2,10 +2,12 @@ package authuser
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 class AuthuserServiceApplication
 
 fun main(args: Array<String>) {
