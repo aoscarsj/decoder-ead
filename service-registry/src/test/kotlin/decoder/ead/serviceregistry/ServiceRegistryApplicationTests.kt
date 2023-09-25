dfb8915a78d322787c80dd2ae3760058c5a6cdfb
Artur Oscar <Arturoscar3@gmail.com>
2023-09-25 20:20:17 -0300

    [service-registry]: Adding a service registry with netflix eureka server.

---
+++ service-registry/src/test/kotlin/decoder/ead/serviceregistry/ServiceRegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package decoder.ead.serviceregistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
