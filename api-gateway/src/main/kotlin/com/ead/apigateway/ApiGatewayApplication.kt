ec731796c811e17579e5d61cd89b7c85b2e90b56
Artur Oscar <Arturoscar3@gmail.com>
2023-10-03 21:05:16 -0300

    [api-gateway]: Insert api-gateway service.

---
+++ api-gateway/src/main/kotlin/com/ead/apigateway/ApiGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.ead.apigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiGatewayApplication>(*args)
+}
