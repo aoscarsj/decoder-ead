04bf764bc51b2b7633d1337607b1ddd9c9f2f182
Artur Oscar <Arturoscar3@gmail.com>
2023-10-25 22:01:27 -0300

    [course]: improving exceptions in rest integration with authuser.

--- course-service/src/main/kotlin/course/core/course/helper/UserHelper.kt
+++ course-service/src/main/kotlin/course/core/course/helper/UserHelper.kt
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.stereotype.Component
 import java.util.*
@@ -26,6 +27,13 @@ class UserHelper(
             userClient.findAllUsersByCourse(courseId, page).response ?: throw CourseException(
                 "User not found by course #$courseId", NOT_FOUND
             )
+        } catch (e: FeignException) {
+            logger.error("Error in finding users by course")
+            logger.error(e.stackTrace)
+
+            if (e.status() == 404)
+                throw CourseException("User not found by course #$courseId", NOT_FOUND)
+            throw RestException(INTERNAL_SERVER_ERROR, "Error on fetching user information")
         } catch (e: Exception) {
 
             logger.error("Error in finding users by course {}", e.stackTrace)
@@ -37,6 +45,10 @@ class UserHelper(
 
         try {
             userClient.sendSubscription(userId, UserSubscriptionRequest(courseId))
+        } catch (e: FeignException) {
+
+            logger.error("Error in sending subscription - Feign {}", e.stackTrace)
+            throw RestException(INTERNAL_SERVER_ERROR, "Error in sending subscription")
         } catch (e: Exception) {
 
             logger.error("Error in sending subscription {}", e.stackTrace)
@@ -52,7 +64,7 @@ class UserHelper(
     } catch (e: FeignException) {
 
         logger.error("Error in fetching user #$userId {}", e.localizedMessage)
-        if (e.localizedMessage.contains("NOT_FOUND"))
+        if (e.status() == 404)
             throw RestException(NOT_FOUND, "Error: User not found.")
         throw e
     } catch (e: Exception) {
